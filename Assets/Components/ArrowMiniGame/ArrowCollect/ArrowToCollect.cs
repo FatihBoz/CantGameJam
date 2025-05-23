cde174dd37467ca2358dc92af96082992b9dadd5
Fatih <faatihboz@gmail.com>
2025-05-24 02:25:20 +0300

    Arrow Collect mini game arrow gathering in box remained

--- Assets/Components/ArrowMiniGame/ArrowCollect/ArrowToCollect.cs
+++ Assets/Components/ArrowMiniGame/ArrowCollect/ArrowToCollect.cs
@@ -1,23 +1,43 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ArrowToCollect : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    [Header("Arrow Sprites")]
+    [SerializeField] private Sprite initialBlueSprite;
+    [SerializeField] private Sprite initialRedSprite;
+    [SerializeField] private Sprite wholeRedSprite;
+    [SerializeField] private Sprite wholeBlueSprite;
+
     private ArrowDirection arrowDirection = ArrowDirection.Up;
     private Vector3 lastPressPos;
-    private int rotationNeedToGather = 3;
+    private int rotationNeedToGather = 7;
     private bool isDragging = false;
     private float rotateAmount = 30f;
     private float initialRotationZ;
 
     private ArrowCollectMiniGame miniGame;
     private Transform parentTransform;
+    bool isHolding;
+    int r;
 
     private void Start()
     {
+        List<Sprite> arrowSprites = new() { initialBlueSprite, initialRedSprite};
+        r = Random.Range(0, arrowSprites.Count);
+        if(gameObject.TryGetComponent<Image>(out Image image))
+        {
+            image.sprite = arrowSprites[r];
+        }
+
+
         miniGame = GetComponentInParent<ArrowCollectMiniGame>();
         initialRotationZ = transform.localRotation.eulerAngles.z;
         parentTransform = transform.parent;
+        initialRotationZ = parentTransform.localRotation.eulerAngles.z;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -28,7 +48,7 @@ public class ArrowToCollect : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging)
+        if (isDragging && !isHolding)
         {
             if (Input.mousePosition.y < lastPressPos.y && arrowDirection == ArrowDirection.Up)
             {
@@ -53,9 +73,21 @@ public class ArrowToCollect : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
                 {
                     print("MiniGame is null");
                 }
+
+                List<Sprite> arrowSprites = new() { wholeBlueSprite, wholeRedSprite };
+                if (gameObject.TryGetComponent<Image>(out Image image))
+                {
+                    image.sprite = arrowSprites[r];
+                }
+
+
                 miniGame.OnArrowCollected();
-                Debug.Log("Arrow gathered");
+                isHolding = true;
+                transform.parent.AddComponent<DraggableObject>();
+                Destroy(gameObject.GetComponent<ArrowToCollect>());
             }
+            
+             
 
         }
 
@@ -65,6 +97,7 @@ public class ArrowToCollect : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
     {
         isDragging = false;
     }
+
 }
 
 
